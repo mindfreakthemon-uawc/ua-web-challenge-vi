dc08c76916e75862139c6bdec8e762f95bcc49a4
Vitalii Rakovtsii <vitalii.rakovtsii@playtech.com>
2014-05-06 18:02:50 +0300

    Added stylus and jade files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,35 @@
 module.exports = function (grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		watch: {},
 		jshint: {
 			main: [
 				'Gruntfile.js',
 				'js/app/**/*.js'
 			]
+		},
+		stylus: {
+			main: {
+				expand: true,
+				cwd: 'stylus',
+				src: ['*.styl'],
+				dest: 'css/app',
+				ext: '.css'
+			}
+		},
+		watch: {
+			css: {
+				files: ['stylus/**/*.styl'],
+				tasks: ['stylus'],
+				options: {
+					livereload: true
+				}
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-stylus');
 	grunt.loadNpmTasks('grunt-contrib-jade');
 	grunt.registerTask('test', 'jshint');
 	grunt.registerTask('default', []);
